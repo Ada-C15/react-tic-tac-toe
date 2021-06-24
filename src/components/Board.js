5c9e3812b9f9fa65c461d750ad900d39da6bc9c0
melissa-nguyen <n.melissa.25@gmail.com>
2021-06-23 21:12:47 -0700

    Modified generateSquareComponents in board.js, flattened squares array from 2D too 1D. Mapped indexes of 1D array to set propTypes and values

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,14 +3,33 @@ import './Board.css';
 import Square from './Square';
 import PropTypes from 'prop-types';
 
-
+// squares param comes from the return value of generateSquares func from App.js
 const generateSquareComponents = (squares, onClickCallback) => {
+
+  //  empty list variable, will hold the value of the flattened 2D->1D array
+  let squareOneD = [];
+
+  // create for loop to iterate through each index-the length of squares (2D array)
+  // concatenate the values squares and add it to the empty 1D variable
+  for (let i = 0; i < squares.length; i++)
+  {
+    squareOneD = squareOneD.concat(squares[i]);
+  };
+  console.log(squareOneD);
+
+  // map iterates through each index of squareOneD
+  //  it sets the values to the props: id, value and onClickCallback
+  return squareOneD.map((square, index) => {
+    return <Square key={index} id={square.id} value={square.value} onClickCallback={onClickCallback}></Square>
+    console.log(`this makes the square grid`);
+  });
+  
   // Complete this for Wave 1
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
 
-}
+};
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
@@ -18,7 +37,7 @@ const Board = ({ squares, onClickCallback }) => {
   return <div className="grid" >
     {squareList}
   </div>
-}
+};
 
 Board.propTypes = {
   squares: PropTypes.arrayOf(
--- src/components/Square.js
+++ src/components/Square.js
@@ -6,7 +6,9 @@ import './Square.css'
 const Square = (props) => {
   // For Wave 1 enable this 
   //  Component to alert a parent 
-  //  component when it's clicked on.
+  //  component when it's clicked on.]
+
+
 
   return <button
     className="square"
