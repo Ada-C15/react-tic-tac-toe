1da64232c7dbff0b7c937ee107ee62db7f15fbe0
Glenda <chicag9313@gmail.com>
2021-06-24 22:38:42 -0700

    Pass wave 1,2,3

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,16 +5,33 @@ import PropTypes from 'prop-types';
 
 
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
+  // console.log(onClickCallback)
+ // Complete this for Wave 1
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
 
+  let newArr = [];
+  for(let i = 0; i < squares.length; i++)
+  {
+      newArr = newArr.concat(squares[i]);
+  }
+
+  let squaresArr = [];
+  for(let i = 0; i < newArr.length; i++)
+  {
+      const currentSquare = newArr[i];
+      squaresArr.push ( <Square value={currentSquare.value} id={currentSquare} onClickCallback={onClickCallback}/>)
+      // console.log('Here issss',{currentSquare})
+  } 
+  return squaresArr
+  
 }
 
 const Board = ({ squares, onClickCallback }) => {
+  // console.log(onClickCallback)
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
+  // console.log(squareList);
   return <div className="grid" >
     {squareList}
   </div>
