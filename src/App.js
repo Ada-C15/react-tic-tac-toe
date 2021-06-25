5e80ff171fe8354614026f22dbc6d8705cb7d6d8
Tammy <iptmhatuw@gmail.com>
2021-06-25 00:06:12 -0700

    updates all waves

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Board from './components/Board';
 
-const PLAYER_1 = 'X';
-const PLAYER_2 = 'O';
+const PLAYER_1 = 'x';
+const PLAYER_2 = 'o';
 
 const generateSquares = () => {
   const squares = [];
@@ -30,11 +30,28 @@ const App = () => {
   // This starts state off as a 2D array of JS objects with
   // empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
+  const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
 
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
+
+  const updateSquare = (id) => {
+    // make a new array of squares to map out each individual square
+    const newSquares = squares.map((row) => {
+      return row.map((square) => {
+        // if square matches the id that got passed in and not empty
+        if (square.id === id && !square.value) {
+          square.value = currentPlayer
+          setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1)
+        }
+        return square
+      })
+    })
+    setSquares(newSquares)
+    checkForWinner()
+  }
 
 
   const checkForWinner = () => {
@@ -48,6 +65,73 @@ const App = () => {
     // 3. Go across each diagonal to see if 
     //    all three squares have the same value.
 
+    // These are all the possibilities in the board to win
+    // const lines = [
+    //   [0,1,2],
+    //   [3,4,5],
+    //   [6,7,8],
+    //   [0,3,6],
+    //   [1,4,7],
+    //   [2,5,8],
+    //   [0,4,8],
+    //   [2,4,6],
+    // ];
+    // for (let i = 0; i < lines.length; i++){
+    //   const[a,b,c] = lines[i];
+    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+    //     return squares[a];
+    //   }
+    // }
+    // return null;
+
+    // Columns
+    if (squares[0][0].value === squares[1][0].value && squares[1][0].value === squares[2][0].value && squares[0][0].value !== '') {
+      return squares[0][0].value;
+    }
+
+    if (squares[0][1].value === squares[1][1].value && squares[1][1].value === squares[2][1].value && squares[0][1].value !== '') {
+      return squares[0][1].value;
+    }
+
+    if (squares[0][2].value === squares[1][2].value && squares[1][2].value === squares[2][2].value && squares[0][2].value !== '') {
+      return squares[0][2].value;
+    }
+
+    // Rows
+    if (squares[0][0].value === squares[0][1].value && squares[0][1].value === squares[0][2].value && squares[0][0].value !== '') {
+      return squares[0][0].value;
+    }
+
+    if (squares[1][0].value === squares[1][1].value && squares[1][1].value === squares[1][2].value && squares[1][0].value !== '') {
+      return squares[1][0].value;
+    }
+
+    if (squares[2][0].value === squares[2][1].value && squares[2][1].value === squares[2][2].value && squares[2][0].value !== '') {
+      return squares[2][0].value;
+    }
+
+    // Diagonals
+    if (squares[0][0].value === squares[1][1].value && squares[1][1].value === squares[2][2].value && squares[0][0].value !== '') {
+      return squares[0][0].value;
+    }
+
+    if (squares[0][2].value === squares[1][1].value && squares[1][1].value === squares[2][0].value && squares[0][2].value !== '') {
+      return squares[0][2].value;
+    }
+
+    // If conditions are false above and there is a space empty on the board, game still in progress 
+
+    // for (let row = 0; row < 3; row += 1) {
+    //   for (let col = 0; col < 3; col += 1) {
+    //     if (squares[row][col].value === '') {
+    //       return '';
+    //     }
+    //   }
+    // }
+    if (squares.flat().includes('')) {  
+      return null;
+    }
+    return 'Tie';
   }
 
   const resetGame = () => {
@@ -58,11 +142,11 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>Winner is {checkForWinner()} </h2>
         <button>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback={updateSquare}/>
       </main>
     </div>
   );
