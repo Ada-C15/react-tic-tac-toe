8ed813f836f780ffe058a8a750dda816b6a8a556
melissa-nguyen <n.melissa.25@gmail.com>
2021-06-25 08:08:18 -0700

    Cleaned up comments and refactored code

--- src/App.js
+++ src/App.js
@@ -27,28 +27,16 @@ const generateSquares = () => {
 
 const App = () => {
 
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
+  //  State for keeping count of which square index has been clicked:
   const [squares, setSquares] = useState(generateSquares());
-  //  State: 1 for keeping count of which square index has been clicked,
-  //  1 for keeping track of which player_1 (X) or player_2 (O) is clicking which square
-  //  1 for how many squares are filled, the game has to stop once 9 squares have been filled
-  //  1 for the winner, to set either player_1 or player_2 equal to winner 
-
+  
+  //  State for keeping track of which player_1 (X) or player_2 (O) is clicking which squareL
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+  
+  //  1 for the winner, to set either player_1 or player_2 equal to winner:
   const [winner, setWinner] = useState(null);
 
   const checkForWinner = () => {
-    // Complete in Wave 3
-    // You will need to:s
-    // 1. Go accross each row to see if 
-    //    3 squares in the same row match
-    //    i.e. same value
-    // 2. Go down each column to see if
-    //    3 squares in each column match
-    // 3. Go across each diagonal to see if 
-    //    all three squares have the same value.
-
     // check (if) winner in row and (elif) winner in column:
     for (let i = 0; i < 3; i++) {
       if (squares[i][0].value === squares[i][1].value && squares[i][1].value === squares[i][2].value && squares[i][0].value !== '') {
@@ -64,27 +52,19 @@ const App = () => {
     } else if (squares[0][2].value === squares[1][1].value && squares[1][1].value === squares[2][0].value && squares[0][0].value !== '') {
       return squares[0][2].value;
     } 
-    // create a loop, count the blank squares, if there are 0 blank squares -> tie 
-    // for (let i = 0; i < 3; i++) {
 
-    // } 
+    // check for tie and null:
+
     for (let r = 0; r < 3; r++) {
       for (let c = 0; c < 3; c++) {
         if (squares[r][c].value === '') {
           return null;
         }
       }}
-
-  
-    //  returning  null if there are still empty spaces
     return 'Tie';
-
-    // last else -> return tie if all spaces are filled but there's no winner 
-
   };
 
   // Wave 2
-  // You will need to create a method to change the square When it is clicked on then pass it into the squares as a callback
   const updateSquares = (id) => {
 
     if (winner === PLAYER_1 || winner === PLAYER_2) return;
@@ -102,7 +82,6 @@ const App = () => {
     setWinner(checkForWinner());
   };
 
-  
 
   const resetGame = () => {
     // Complete in Wave 4
