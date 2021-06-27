8a69892ea0d4dbc5133a29eb35463ac53920d217
Andrea Palacios <andreapalacios88@gmail.com>
2021-06-27 14:28:11 -0700

    all tests passing

--- src/App.js
+++ src/App.js
@@ -34,28 +34,27 @@ const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
   const [player, setPlayer]= useState(PLAYER_1);
-  const [status, setStatus] = useState('Lets play');
+  const [status, setStatus] = useState('Let\'s play');
   const [winner, setWinner] = useState(null);
   const [turnCount, setTurnCount]=useState(0)
 
-
-    /*updateSquares is working*/
+    
   const updateSquares=(id)=>{
-    if (winner === null){
+    if (winner !== null) return;
   
     const newSquares = [...squares];
-    let selectedSquare = newSquares.flat().find( newSquare =>newSquare.id ===id)
+    let selectedSquare = newSquares.flat().find( newSquare =>newSquare.id ===id);
   
     if (selectedSquare.value !== '') return;
       selectedSquare.value = player;
-      updateTurn();
-      updateStatus();
 
     setSquares(newSquares);
+    updateTurn();
+    updateStatus(); 
   };
-}
+
   
-  const checkForWinner = () => {
+  const checkForWinner = () => {{
     // vertical cases
     for (let i = 0; i < 3; i++) {
       if (squares[0][i].value === squares[1][i].value &&
@@ -85,32 +84,34 @@ const App = () => {
       squares[0][2].value !== '') {
         return squares[0][2].value
       }
-
-    return null;
   }
+  return null;
+  };
   
   /*update turn is working*/
   const updateTurn = ()=> {
     if (player === PLAYER_1) {
-      setPlayer(PLAYER_2)
-      setTurnCount(turnCount+1)
+      setPlayer(PLAYER_2);
+      setTurnCount(turnCount+1);
     } else if (player === PLAYER_2) {
-      setPlayer(PLAYER_1)
-      setTurnCount(turnCount + 1)
+      setPlayer(PLAYER_1);
+      setTurnCount(turnCount + 1);
       }
-    setWinner(checkForWinner());
     };
 
+
   const updateStatus= () => {
-    if (winner === null && turnCount === 9) {
-        setStatus('It\'s a tie!')
+    const winner = (checkForWinner())
+    if (winner === null && turnCount === 8) {
+      setStatus('Oh no...It\'s a tie!')
     }
     else if (winner === null) {
-      setStatus(`Current Player ${player}`)
+      setStatus(`Current Player is ${player}`)
       }
     else if (winner === 'x' || winner === 'o') {
       setStatus(`Winner is ${winner}`)
       }
+    return winner
     }
 
   /*reset Game is passing*/
@@ -119,13 +120,14 @@ const App = () => {
     setPlayer(PLAYER_1);
     setStatus('Let\'s Play!');
     setWinner(null);
+    setTurnCount(0)
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>(updateStatus())</h2>
+        <h2>{status}</h2>
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
