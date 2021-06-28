7f6a6f13a92d7a2108c3e54122ce1b39597bd9e0
Alli-Oops <AllisonLeonardSeattle@gmail.com>
2021-06-27 19:04:37 -0700

    updated the check for winner functions. Still failing tests

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ const App = () => {
                                                       // ^ creates the initial state for squares - provided by the function generateSquares()
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
                                                       // ^ create an initial state for who's turn 
+  const defaultWinnerString = 'The winner is ...'
 
+  const [winnerString, setWinnerString] = useState(defaultWinnerString)
   // Wave 2
   
   const onClickCallback = (event) => {
@@ -51,6 +53,7 @@ const App = () => {
       }
 
       setSquares(newSquares)
+      checkForWinner(newSquares)
 
       // const setCurrentPlayer = PLAYER_1 ? PLAYER_2: PLAYER_1;
       // OR //
@@ -65,16 +68,62 @@ const App = () => {
   // onClickCallback is the name of a property that belongs to the board  - but the function OnClickCallback is what it will do
   
   
-  // const checkForWinner = () => {
+  const checkForWinner = (arr) => {
     // Complete in Wave 3
     // You will need to:
     // 1. Go accross each row to see if 
     //    3 squares in the same row match
     //    i.e. same value
+    for (let r = 0; r < 3; r++) {
+      const winnerExists = determineWinner(arr[r])
+      if (winnerExists) {
+        return
+      }
+    }
+
     // 2. Go down each column to see if
     //    3 squares in each column match
+
+    for (let c = 0; c < 3; c++) {
+      const col = []
+      for (let r = 0; r < 3; r++) {
+        col.push(arr[r][c].value)
+      }
+      const winnerExists = determineWinner(col)
+      if (winnerExists) {
+        return
+      }
+    }
     // 3. Go across each diagonal to see if 
     //    all three squares have the same value.
+    const diag1 = []
+    for (let d = 0; d < 3; d++) {
+      diag1.push(arr[d][d].value)
+    }
+
+    const diag2 = []
+    for (let d = 0; d < 3; d++) {      
+      diag2.push(arr[d][2-d].value)
+    }
+    const diag2WinnerExists = determineWinner(diag2)
+    if (diag2WinnerExists) {
+      return
+    }
+  }
+
+  const determineWinner = (arr) => {
+    if (arr.every(val => val === PLAYER_1 )) {
+      setWinnerString(`Winner is ${PLAYER_1}`)
+      return true
+    } else if (arr.every(val => val === PLAYER_2 )) {
+      setWinnerString(`Winner is ${PLAYER_2}`)
+      return true
+    } else {
+      setWinnerString(defaultWinnerString)
+      return false
+    }
+  }
+
 
   const resetGame = () => {
     // Complete in Wave 4
@@ -84,7 +133,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>The winner is ...  </h2>
         <button>Reset Game</button>
       </header>
       <main>
