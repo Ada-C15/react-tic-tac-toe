6329fb0434eb2b3a31d118f78d186a76cf23dafb
Mai Truong <truong.mt89@gmail.com>
2021-06-28 17:37:31 -0700

    squares/grid

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,8 +9,21 @@ const generateSquareComponents = (squares, onClickCallback) => {
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
-
+  let squareBox = [];
+  //loop through index, length of squares (2D)
+  for (let i = 0; i < squares.length; i++)
+  {
+    squareBox = squareBox.concat(squares[i]);
+  };
 }
+  console.log(squareBox);
+  //iterate through each index of squareBoxOne
+  return squareBox.map((square, index) => {
+    return <Square key={index} id={square.id} value={square.value}
+    onClickCallback={onClickCallback} />
+    console.log(`this is the grid`);
+  });
+
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
