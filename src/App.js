777bb532d58ef992fabe93786ae3ddddf2673b77
KARLA TORRES <t.karla212@gmail.com>
2021-07-06 09:25:27 -0400

    Made necessary updates for tests to run by removing .skip on three files.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Board from './components/Board';
 
-const PLAYER_1 = 'X';
-const PLAYER_2 = 'O';
+const PLAYER_1 = 'x';
+const PLAYER_2 = 'o';
 
 const generateSquares = () => {
   const squares = [];
@@ -115,7 +115,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        {winner!==''&&<h2>The winner is {winner} </h2>}
+        {winner!==''&&<h2> Winner is {winner} </h2>}
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
